3a6bfdf4da6f44dca26a3705db2e6e07bf347464
Adedaramola <paranok2017@gmail.com>
2022-08-29 14:39:39 +0100

    implemented whatsapp api resource

--- src/Http/Resources/WhatsappResource.php
+++ src/Http/Resources/WhatsappResource.php
@@ -4,25 +4,27 @@ declare(strict_types=1);
 
 namespace Adedaramola\Sendchamp\Http\Resources;
 
+use Adedaramola\Sendchamp\Http\Requests\WhatsappLocationRequest;
+use Adedaramola\Sendchamp\Http\Requests\WhatsappMessageRequest;
+
 final class WhatsappResource extends Resource
 {
-    public function sendMessage()
+    // this works for text, video, audio and sticker messages
+    public function sendMessage(WhatsappMessageRequest $request)
     {
+        $response = $this->client->post('/whatsapp/message/send', $request->toArray());
+
+        $this->throwFailedErrorResponse($response);
+
+        return $response->object();
     }
 
-    public function sendVideo()
+    public function sendLocation(WhatsappLocationRequest $request)
     {
-    }
+        $response = $this->client->post('/whatsapp/message/send', $request->toArray());
 
-    public function sendSticker()
-    {
-    }
+        $this->throwFailedErrorResponse($response);
 
-    public function sendAudio()
-    {
-    }
-
-    public function sendLocation()
-    {
+        return $response->object();
     }
 }
